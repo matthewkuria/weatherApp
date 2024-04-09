c03be0caed6a0d298103a1021b001ea471e882b1
matthewkuria <mathewkwachira@gmail.com>
2024-04-09 19:02:54 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-full lg;h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
+    <h1>Weather Forecast </h1>
+    <p>Enter below a place you want to know the weather of and select an option from the dropdown</p>
+      </section>
+
+    </main>
+  )
+}
+
+export default App
