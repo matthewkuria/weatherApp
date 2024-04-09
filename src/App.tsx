dd74c927e8894fc559faca685ae8254b30e2e54e
matthewkuria <mathewkwachira@gmail.com>
2024-04-09 19:59:18 +0300

    add functionality to the search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-const App = () => {
+import { useState } from "react"
+
+const App = ():JSX.Element => {
+  const [term, setTerm]=useState('');
+  const onInputChange =(e)=>{
+    console.log(e.target.value);
+
+  }
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-full lg;h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
@@ -7,11 +15,16 @@ const App = () => {
      </h1>
      <p className="text-sm mt-2">Enter below a place you want to know the weather of and select an option from the dropdown</p>
 
-      <input type="text" 
-      value={""}
-       className="px-2 py-1 rounded-l-md border-white"
-       />
-       <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-1 py-1 px-2"></button>
+     <div className="flex mt-10 md:mt-4">
+        <input type="text" 
+          value={term}
+          onChange={onInputChange}
+          className="px-2 py-1 rounded-l-md border-white"
+          />
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-1 py-1 px-2 cursor-pointer">
+            search
+          </button>
+     </div>
 
       </section>
     </main>
