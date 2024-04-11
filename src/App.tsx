922b3eb3b587abb1b496cae59902ca2cdaadff13
matthewkuria <mathewkwachira@gmail.com>
2024-04-11 22:49:58 +0300

    use the useEffect to reset the options array

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { optionType } from "./types";
 
 const App = ():JSX.Element => {
   // create a state variable "term" to store the value of the search box
   const [term, setTerm]=useState<string>('');
   // Create a change event with the annotations as seen below
+  const[city, setCity] = useState<optionType | null>(null)
   const [options, setOptions] = useState<[]>([]);
 
   const getSearchOptions =(value: string) =>{
@@ -25,12 +26,19 @@ const App = ():JSX.Element => {
     }
     const onOptionSelect = (option:optionType) =>{
       // console.log(option.name)
+      setCity(option)
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric
       &appid=${process.env.REACT_APP_API_KEY}`)
       .then((res) => res.json())
       .then((data)=>console.log({data}))
 
     }
+    useEffect(()=>{
+if(city){
+  setTerm(city.name)
+  setOptions([])
+}
+    },[city])
   // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
